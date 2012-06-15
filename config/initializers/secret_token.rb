cdb69d2ef35806043ce59d271d980b0315333f52
NeeCee Keels <jkeels@unow.com>
2012-06-14 17:02:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FistApp::Application.config.secret_token = 'de346e8d73d7fa4ce24255b4fbfdb898e3d57703dd7ba0183a28655d1b5a5023e6121b85113d990508514d380e371fa44d3a3567a3ac78886ca21f5369dc2850'
